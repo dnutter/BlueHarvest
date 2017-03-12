884bbe33f4ad94cfba32ed09a4a1de87d7e0ead4
David Nutter <dbnutter@gmail.com>
2017-03-11 23:12:05 -0700

    Initial Commit

---
+++ BlueHarvest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlueHarvest
+//
+//  Created by David Nutter on 3/11/17.
+//  Copyright © 2017 David Nutter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
